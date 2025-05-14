46dfe14f52acad9596ce805db970cfbed5b26418
VerifiedAnon <verifiedanon@sodahq.com>
2025-05-14 11:08:49 -0400

    fixed inputs

---
+++ json.js
@@ -0,0 +1,3 @@
+const jsonString = '{"myLargeNumber": 9826231912286180120828980649989613944510478291528692173128872149148539351640}';
+const parsedObject = JSON.parse(jsonString);
+console.log(parsedObject.myLargeNumber);
