e29cb4a0b96d1a4179ad778f6898ad8f30108e14
VerifiedAnon <verifiedanon@sodahq.com>
2025-05-15 09:10:20 -0400

    fixes

--- json.js
+++ json.js
@@ -1,3 +1,4 @@
-const jsonString = '{"myLargeNumber": 9826231912286180120828980649989613944510478291528692173128872149148539351640}';
+const jsonString = '{"myLargeNumber": "9826231912286180120828980649989613944510478291528692173128872149148539351640"}';
 const parsedObject = JSON.parse(jsonString);
 console.log(parsedObject.myLargeNumber);
+
